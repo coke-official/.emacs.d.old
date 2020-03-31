ba7fc40f1eb5eb91bc32369f4ab62645b0c2e9ca
Yu Matsumura <matsumura@mizuuchi.lab.tuat.ac.jp>
2020-03-31 15:21:46 +0900

    emacs setting files

---
+++ init.el
@@ -0,0 +1,30 @@
+;;;cask
+
+;(package-initialize)
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;;; init-loader
+(let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (virtualenv auto-virtualenv auto-virtualenvwrapper virtualenvwrapper ansi package-build shut-up epl git commander f dash s))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
