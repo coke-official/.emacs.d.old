04d5877c09823f85a1152b7c6d4b3b15ae3ba78f
coke-official <cokeofficial1886@gmail.com>
2020-12-26 20:44:17 +0900

    気分

--- inits/13-web.el
+++ inits/13-web.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
 
 (defun web-mode-hook ()
   "Hooks for Web mode."
